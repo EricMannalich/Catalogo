93d969c5521088d4bb7843c3297f0aa676be639e
EricMannalich <78314783+EricMannalich@users.noreply.github.com>
2021-12-29 13:25:13 -0500

    administracion y permisos

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext, gettext_lazy as _
 class UsuarioAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'image')}),
         (_('Permissions'), {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
